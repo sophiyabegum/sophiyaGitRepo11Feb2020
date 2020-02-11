fb042aa50c83e620660194900b780fa26ba48145
sophiya <sophiya@websynergies.biz>
2020-02-11 15:13:40 +0530

    Thrid commit

---
+++ SophiyaGitDevOpsApp11Feb020/Controllers/SophiyaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SophiyaGitDevOpsApp11Feb020.Controllers
+{
+    public class SophiyaController : Controller
+    {
+        // GET: Sophiya
+        public ActionResult SophiyaGithub()
+        {
+            return View();
+        }
+    }
+}
